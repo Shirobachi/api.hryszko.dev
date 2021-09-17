f8b1f45d50f80561e24904b8304df72a1b5bd7e9
Simon Hryszko <simon@hryszko.dev>
2021-09-17 16:09:48 +0200

    Added /about path

--- app/main.py
+++ app/main.py
@@ -10,3 +10,10 @@ async def root():
 @app.get("/isEven/{number}")
 async def isEven(number: int):
     return {"number": number, "isEven": number % 2 == 0}
+
+@app.get("/about")
+async def about():
+	return { 	"Author": "Simon Hryszko", 
+				"Email": "simon@hryszko.dev",
+				"Github": "shirobachi",
+				"Description": "This is official hryszko.dev API" }
