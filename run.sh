771bae20da0931f0c0d7f9a63a39bd0c322fa232
Simon Hryszko <simon@hryszko.dev>
2021-09-30 00:10:38 +0200

    Small fixes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-set -x
+# set -x
 trap "clear && $0" SIGINT
 
 # if parameter is given -c or --clean-up, then clean up
@@ -18,18 +18,21 @@ if [ "$1" = "-c" ] || [ "$1" = "--clean-up" ]; then
 	exit 0
 fi
 
-while true; do
-	# Reading environment variables from .env file
-	envs=""
-	for i in $(seq $(cat app/.env | wc -l)); do
-		left=$(cat app/.env | sed -n "$i p" | cut -d '=' -f 1)
-		right=$(cat app/.env | sed -n "$i p" | cut -d '=' -f 2)
+# if 0 parameters
+if [ $# -eq 0 ]; then
+	while true; do
+		# Reading environment variables from .env file
+		envs=""
+		for i in $(seq $(cat app/.env | wc -l)); do
+			left=$(cat app/.env | sed -n "$i p" | cut -d '=' -f 1)
+			right=$(cat app/.env | sed -n "$i p" | cut -d '=' -f 2)
 
-		envs="$envs -e $left=$right"
+			envs="$envs -e $left=$right"
+		done
+
+		# Run the app
+		docker build -t shirobachi/api.hryszko.dev . &&
+		docker run -p 8000:80 $envs $(docker images | head -2 | tail -1 | awk '{print $3}') ||
+		echo "Error: Cannot run the app"; exit 1
 	done
-
-	# Run the app
-	docker build -t shirobachi/api.hryszko.dev . &&
-	docker run -p 8000:80 $envs $(docker images | head -2 | tail -1 | awk '{print $3}') ||
-	echo "Error: Cannot run the app"; exit 1
-done
+fi
