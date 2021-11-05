2a9e52184d879c1f091f1f5ac975129dc5badc34
Simon Hryszko <simon@hryszko.dev>
2021-11-06 00:44:42 +0100

    Fix models, token checker does not return pass:null

--- app/routers/users.py
+++ app/routers/users.py
@@ -9,13 +9,19 @@ import jwt
 
 router = APIRouter()
 
-
+# Models
 class User(BaseModel):
 	login: str
-	password: Optional[str]
+	password: str
+class UserNoPass(BaseModel):
+	login: str
+
+class Token(BaseModel):
+	token: str
+
 
 # Register new person
-@router.post("/", tags=["users"])
+@router.post("/", response_model=UserNoPass, tags=["users"])
 async def register(user: User):
 	Collection = db["users"]
 	# TODO: validation (password:tooShort|tooLong)
@@ -24,57 +30,24 @@ async def register(user: User):
 	# Check if login has special chars
 	print (user.login, user.login.isalnum())
 
-	if not user.login.isalnum():
-		raise HTTPException(
-            status_code=400,
-            detail="Login has special chars",
-        )
+	if not user.login.isalnum(): # if login contain a-zA-Z0-9
+		raise HTTPException( status_code=400, detail="Login has special chars")
+	if len(user.login) < 3 or len(user.login) > 20: # if login not b/w 3-20 chars
+		raise HTTPException( status_code=400, detail="Login must be between 3 and 20 characters")
+	if Collection.find_one({"login": user.login}): # Check if login already exist
+		raise HTTPException( status_code=400, detail="Login taken")
+	if not any(char.isdigit() for char in user.password): # check if password has at least one number
+		raise HTTPException( status_code=400, detail="Password must contain at least one number")
+	if not any(char.isupper() for char in user.password): # check if password has at least one uppercase letter
+		raise HTTPException( status_code=400, detail="Password must contain at least one uppercase letter")
+	if len(user.password) < 8 or len(user.password) > 100: # check if password is at least 8 chars long and not more than 100
+		raise HTTPException( status_code=400, detail="Password must be between 8 and 100 characters")
 
-	# if login not b/w 3-20 chars
-	if len(user.login) < 3 or len(user.login) > 20:
-		raise HTTPException(
-            status_code=400,
-            detail="Login must be between 3 and 20 characters",
-        )
-
-	# Check if login already exist
-	if Collection.find_one({"login": user.login}):
-		raise HTTPException(
-            status_code=400,
-            detail="Login has special chars",
-        )
-
-	# VALIDATION - PASSWORD
-	# check if password has at least one number
-	if not any(char.isdigit() for char in user.password):
-		raise HTTPException(
-            status_code=400,
-            detail="Password must contain at least one number",
-        )
-
-	# check if password has at least one uppercase letter
-	if not any(char.isupper() for char in user.password):
-		raise HTTPException(
-            status_code=400,
-            detail="Password must contain at least one uppercase letter",
-        )
-
-	# check if password is at least 8 chars long and not more than 100
-	if len(user.password) < 8 or len(user.password) > 100:
-		raise HTTPException(
-            status_code=400,
-            detail="Password must be between 8 and 100 characters",
-        )
 
 	user.password = generate_password_hash(user.password)
 	Collection.insert_one(user.dict())
 
-	return {
-		"message": "User created"
-	}
-
-class Token(BaseModel):
-	token: str
+	return user
 
 
 # Generate token JWT
@@ -101,7 +74,7 @@ async def login(user: User):
 
 
 # verify token
-@router.get("/token/{token}", response_model=User, tags=["users"])
+@router.get("/token/{token}", response_model=UserNoPass, tags=["users"])
 async def verify_token(token: str):
 	try:
 		r = jwt.decode(token, JWT_SECRET_KEY, algorithms=["HS256", ])
