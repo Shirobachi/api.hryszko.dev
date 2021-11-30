b61d9d7a58d0d9e191c7c5c2180a44190b882f7b
Simon Hryszko <simon@hryszko.dev>
2021-11-30 11:23:11 +0100

    Fix tests method

--- app/tests/test_tictactoe.py
+++ app/tests/test_tictactoe.py
@@ -29,7 +29,7 @@ def test_game_state_422_2():
 # == MAKE MOVE ==
 def test_make_move_422_1():
 	response = client.post('/tictactoe/')
-	response = client.post('/tictactoe/' + str(response.json()['code']) + "/dasd")
+	response = client.put('/tictactoe/' + str(response.json()['code']) + "/dasd")
 
 	assert response.status_code == 422
 
@@ -37,15 +37,15 @@ def test_make_move_422_1():
 def test_draw_1_200_1():
 	tmp = client.post('/tictactoe/')
 
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/4")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/0")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/1")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/7")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/3")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/5")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/6")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/2")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/8")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/4")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/0")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/1")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/7")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/3")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/5")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/6")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/2")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/8")
 
 	assert response.status_code == 200
 	assert response.json()['winner'] == -1
@@ -54,11 +54,11 @@ def test_draw_1_200_1():
 def test_win_row_1_200_1():
 	tmp = client.post('/tictactoe/')
 
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/0")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/3")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/1")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/4")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/2")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/0")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/3")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/1")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/4")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/2")
 
 	print(response.json())
 
@@ -69,11 +69,11 @@ def test_win_row_1_200_1():
 def test_win_row_2_200_1():
 	tmp = client.post('/tictactoe/')
 
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/3")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/6")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/4")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/7")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/5")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/3")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/6")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/4")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/7")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/5")
 
 	print(response.json())
 
@@ -84,11 +84,11 @@ def test_win_row_2_200_1():
 def test_win_row_3_200_1():
 	tmp = client.post('/tictactoe/')
 
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/6")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/4")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/7")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/5")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/8")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/6")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/4")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/7")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/5")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/8")
 
 	print(response.json())
 
@@ -99,11 +99,11 @@ def test_win_row_3_200_1():
 def test_win_col_1_200_1():
 	tmp = client.post('/tictactoe/')
 
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/0")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/1")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/3")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/2")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/6")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/0")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/1")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/3")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/2")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/6")
 
 	print(response.json())
 
@@ -114,11 +114,11 @@ def test_win_col_1_200_1():
 def test_win_col_2_200_1():
 	tmp = client.post('/tictactoe/')
 
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/1")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/0")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/4")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/2")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/7")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/1")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/0")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/4")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/2")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/7")
 
 	print(response.json())
 
@@ -129,11 +129,11 @@ def test_win_col_2_200_1():
 def test_win_col_3_200_1():
 	tmp = client.post('/tictactoe/')
 
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/2")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/4")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/5")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/1")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/8")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/2")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/4")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/5")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/1")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/8")
 
 	print(response.json())
 
@@ -144,11 +144,11 @@ def test_win_col_3_200_1():
 def test_win_diagonal_1_200_1():
 	tmp = client.post('/tictactoe/')
 
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/0")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/1")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/4")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/3")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/8")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/0")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/1")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/4")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/3")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/8")
 
 	print(response.json())
 
@@ -159,11 +159,11 @@ def test_win_diagonal_1_200_1():
 def test_win_diagonal_2_200_1():
 	tmp = client.post('/tictactoe/')
 
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/2")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/1")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/4")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/7")
-	response = client.post('/tictactoe/' + str(tmp.json()['code']) + "/6")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/2")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/1")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/4")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/7")
+	response = client.put('/tictactoe/' + str(tmp.json()['code']) + "/6")
 
 	print(response.json())
 
